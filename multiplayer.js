fe5f6778a670e0c7805c12b4f8bc8549a44584fc
Luís Silva <luisffsilva01@gmail.com>
2020-01-07 11:03:08 +0000

    Disable Buttons

--- multiplayer.js
+++ multiplayer.js
@@ -5,10 +5,12 @@ const words = [
         items: ['cao', 'macaco', 'rato']
     },
     {
-        category: 'paises',
+        category: 'países',
         items: ['portugal', 'espanha']
     }
 ]
+
+
 
 let categoryObject = words[Math.floor(Math.random() * words.length)]
 
@@ -16,12 +18,15 @@ console.log(categoryObject);
 
 let word = categoryObject.items[Math.floor(Math.random() * categoryObject.items.length)].toUpperCase()
 
-document.getElementById("category").innerHTML = categoryObject.category
+document.getElementById("category").innerHTML = categoryObject.category.toUpperCase()
 
 for (let i = 0; i < word.length; i++) {
     document.getElementById("pcWord").innerHTML += `&nbsp_&nbsp`
     console.log(word[i]);
 }
+
+let letterAttempt = []
+
 
 /*imagem forca*/
 
@@ -29,7 +34,7 @@ let contForca = 0
 
 document.querySelector(".forca").src=`img/f${contForca}.png`
 
-
+/*fim imagem forca*/
 
 //teclado
 const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -38,16 +43,17 @@ for (let i = 0; i < letters.length; i++) {
     console.log(letters[i]);
 };
 
+for (let i = 0; i < letterAttempt.length; i++) {
+    document.querySelector(`#letter${letterAttempt[i].toUpperCase()}`).disabled = 'true'
+    
+}
 
+/*player names*/
 
+let pName1
+let pName2
 
-// Selecionar Objeto
-/* let categoryObject = words[Math.floor(Math.random() * words.length)] */
+document.querySelector(".leftP").innerHTML= pName1
+document.querySelector(".rightP").innerHTML= pName1
 
-
-
-/* for (let i = 0; i < word.length; i++) {
-    document.getElementById("pcWord").innerHTML += `_ &nbsp`
-    console.log(word[i]);
-
-} */
+/*fim player names*/
