4ef7980c2709da6110b46d650db0262076bd2271
danielcabral-code <57807464+danielcabral-code@users.noreply.github.com>
2020-01-08 18:17:51 +0000

    8/1

--- multiplayer.js
+++ multiplayer.js
@@ -10,50 +10,45 @@ const words = [
     }
 ]
 
+// Teclado
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+for (let i = 0; i < letters.length; i++) {
+    document.getElementsByClassName('keyboard')[0].innerHTML += `<button class="bLetters" id="letter${letters[i]}">${letters[i]}</button>`
+};
 
+// Função para escolher a palavra
+function randomWordAndCategory () {
 
 let categoryObject = words[Math.floor(Math.random() * words.length)]
-
-console.log(categoryObject);
-
 let word = categoryObject.items[Math.floor(Math.random() * categoryObject.items.length)].toUpperCase()
-
 document.getElementById("category").innerHTML = categoryObject.category.toUpperCase()
 
 for (let i = 0; i < word.length; i++) {
     document.getElementById("pcWord").innerHTML += `&nbsp_&nbsp`
-    console.log(word[i]);
 }
+};
 
-let letterAttempt = []
-
-
-/*imagem forca*/
-
+// Imagem da forca
 let contForca = 0
-
 document.querySelector(".forca").src=`img/f${contForca}.png`
 
-/*fim imagem forca*/
+// Função para tentativas de letras do utilizador
+let letterAttempt = []
 
-//teclado
-const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-for (let i = 0; i < letters.length; i++) {
-    document.getElementsByClassName('keyboard')[0].innerHTML += `<button class="bLetters" id="letter${letters[i]}">${letters[i]}</button>`
-    console.log(letters[i]);
-};
+function letterAttemptDisabled () {
 
 for (let i = 0; i < letterAttempt.length; i++) {
     document.querySelector(`#letter${letterAttempt[i].toUpperCase()}`).disabled = 'true'
-    
+}
 }
 
-/*player names*/
-
+// Jogadores
 let pName1
 let pName2
 
 document.querySelector(".leftP").innerHTML= pName1
-document.querySelector(".rightP").innerHTML= pName1
+document.querySelector(".rightP").innerHTML= pName2
 
-/*fim player names*/
+// Chamar funções
+randomWordAndCategory();
+letterAttemptDisabled();
