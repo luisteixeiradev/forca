561aa077a3ededf00697c0c753a306bae0e75136
Luís Silva <luisffsilva01@gmail.com>
2020-01-09 23:15:45 +0000

    Adiconar ao array tentativas

--- functionsButtons.js
+++ functionsButtons.js
@@ -1,103 +1,132 @@
  function letterA() {
     document.getElementById("letterA").disabled = true;
+    letterAttempt.push('A');
+    console.log(letterAttempt);
+
  }
 
  function letterB() {
     document.getElementById("letterB").disabled = true;
+    letterAttempt.push('B');
+    console.log(letterAttempt);
  }
 
  function letterC() {
     document.getElementById("letterC").disabled = true;
+    letterAttempt.push('C');
  }
 
  function letterD() {
     document.getElementById("letterD").disabled = true;
+    letterAttempt.push('D');
  }
 
  function letterE() {
     document.getElementById("letterE").disabled = true;
+    letterAttempt.push('E');
  }
 
  function letterF() {
     document.getElementById("letterF").disabled = true;
+    letterAttempt.push('F');
  }
 
  function letterG() {
     document.getElementById("letterG").disabled = true;
+    letterAttempt.push('G');
  }
 
  function letterH() {
     document.getElementById("letterH").disabled = true;
+    letterAttempt.push('H');
  }
 
  function letterI() {
     document.getElementById("letterI").disabled = true;
+    letterAttempt.push('I');
  }
 
  function letterJ() {
     document.getElementById("letterJ").disabled = true;
+    letterAttempt.push('J');
  }
 
  function letterK() {
     document.getElementById("letterK").disabled = true;
+    letterAttempt.push('K');
  }
 
  function letterL() {
     document.getElementById("letterL").disabled = true;
+    letterAttempt.push('L');
  }
 
  function letterM() {
     document.getElementById("letterM").disabled = true;
+    letterAttempt.push('M');
  }
 
  function letterN() {
     document.getElementById("letterN").disabled = true;
+    letterAttempt.push('N');
  }
 
  function letterO() {
     document.getElementById("letterO").disabled = true;
+    letterAttempt.push('N');
  }
 
  function letterP() {
     document.getElementById("letterP").disabled = true;
+    letterAttempt.push('O');
  }
 
  function letterQ() {
     document.getElementById("letterQ").disabled = true;
+    letterAttempt.push('P');
  }
 
  function letterR() {
     document.getElementById("letterR").disabled = true;
+    letterAttempt.push('Q');
  }
 
  function letterS() {
     document.getElementById("letterS").disabled = true;
+    letterAttempt.push('R');
  }
 
  function letterT() {
     document.getElementById("letterT").disabled = true;
+    letterAttempt.push('S');
  }
 
  function letterU() {
     document.getElementById("letterU").disabled = true;
+    letterAttempt.push('T');
  }
 
  function letterV() {
     document.getElementById("letterV").disabled = true;
+    letterAttempt.push('U');
  }
 
  function letterW() {
     document.getElementById("letterW").disabled = true;
+    letterAttempt.push('V');
  }
 
  function letterX() {
     document.getElementById("letterX").disabled = true;
+    letterAttempt.push('X');
  }
- 
+
  function letterY() {
     document.getElementById("letterY").disabled = true;
+    letterAttempt.push('Y');
  }
 
  function letterZ() {
     document.getElementById("letterZ").disabled = true;
+    letterAttempt.push('X');
  }
