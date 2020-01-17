50293d26d3f116bee14d82abb2bcf4b6f93df78c
danielcabral-code <57807464+danielcabral-code@users.noreply.github.com>
2020-01-17 18:02:09 +0000

    17.1

--- multiplayer.js
+++ multiplayer.js
@@ -1,6 +1,5 @@
 // Palavras PC
-const words = [
-    {
+const words = [{
         category: 'animais',
         items: ['cao', 'macaco', 'rato']
     },
@@ -11,44 +10,53 @@ const words = [
 ]
 
 // Teclado
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 for (let i = 0; i < letters.length; i++) {
     document.getElementsByClassName('keyboard')[0].innerHTML += `<button class="bLetters" id="letter${letters[i]}" onclick="useLetter('${letters[i]}')">${letters[i]}</button>`
 };
 
 function useLetter(letter) {
     document.getElementById(`letter${letter}`).disabled = true;
-    letterAttempt.push({letter});
- }
+    letterAttempt.push({
+        letter
+    });
+    console.log(word);
+
+    for (let i = 0; i < word.length; i++) {
+        if (word.includes({
+                letter
+            })) {
+            console.log(letter);
+
+        }
+    }
+}
+
+let word = []
 
 // Função para escolher a palavra
-
 let wordVisual = []
 
-function randomWordAndCategory () {
+function randomWordAndCategory() {
 
-let categoryObject = words[Math.floor(Math.random() * words.length)]
-let word = categoryObject.items[Math.floor(Math.random() * categoryObject.items.length)].toUpperCase()
-document.getElementById("category").innerHTML = categoryObject.category.toUpperCase()
+    let categoryObject = words[Math.floor(Math.random() * words.length)]
+    word = categoryObject.items[Math.floor(Math.random() * categoryObject.items.length)].toUpperCase()
+    document.getElementById("category").innerHTML = categoryObject.category.toUpperCase()
+    console.log(typeof word);
 
-for (let i = 0; i < word.length; i++) {
-    /*document.getElementById("pcWord").innerHTML += `&nbsp_&nbsp`*/
-    wordVisual.push(`&nbsp_&nbsp`)
+    for (let i = 0; i < word.length; i++) {
+        wordVisual.push(`&nbsp_&nbsp`)
+    }
+
+    for (let i = 0; i < wordVisual.length; i++) {
+        document.getElementById("pcWord").innerHTML += wordVisual[i]
+    }
+
 }
-
-for (let i = 0; i < wordVisual.length; i++) {
-    
-    document.getElementById("pcWord").innerHTML += wordVisual[i]
-
-}
-}
-
-console.log(wordVisual);
-
 
 // Imagem da forca
 let contForca = 0
-document.querySelector(".forca").src=`img/f${contForca}.png`
+document.querySelector(".forca").src = `img/f${contForca}.png`
 
 // Função para tentativas de letras do utilizador
 let letterAttempt = []
@@ -57,8 +65,8 @@ let letterAttempt = []
 let pName1
 let pName2
 
-document.querySelector(".leftP").innerHTML= pName1
-document.querySelector(".rightP").innerHTML= pName2
+document.querySelector(".leftP").innerHTML = pName1
+document.querySelector(".rightP").innerHTML = pName2
 
 // Chamar funções
 randomWordAndCategory()
