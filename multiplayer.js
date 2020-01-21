f2e0071cdbcf75c6b08c1340383e62925c017535
danielcabral-code <57807464+danielcabral-code@users.noreply.github.com>
2020-01-21 10:36:26 +0000

    21.1

--- multiplayer.js
+++ multiplayer.js
@@ -1,6 +1,17 @@
-// Palavras PC
-const ATTEMPTS_NUMBER = 6
+// Variáveis globais
+// Máximo tentativas de letra
+const MAX_FORCA = 6
+// Jogadores
+let playerName1
+let playerName2
+// Palavra escolhida
+let word = []
+// Palavra escolhida convertida em "_ _"
+let wordVisual = []
+// Contador da forca
+let countForca = 0
 
+// Palavras PC
 const words = [{
         category: 'frameworks',
         items: ['ionic', 'angular', 'react']
@@ -11,18 +22,11 @@ const words = [{
     }
 ]
 
-// Jogadores
-let pName1
-let pName2
-
 // Teclado
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 for (let i = 0; i < letters.length; i++) {
     document.getElementsByClassName('keyboard')[0].innerHTML += `<button class="bLetters" id="letter${letters[i]}" onclick="useLetter('${letters[i]}')">${letters[i]}</button>`
 };
-
-let word = []
-let wordVisual = []
 
 // Função para escolher a palavra
 function randomWordAndCategory() {
@@ -30,7 +34,6 @@ function randomWordAndCategory() {
     let categoryObject = words[Math.floor(Math.random() * words.length)]
     word = categoryObject.items[Math.floor(Math.random() * categoryObject.items.length)].toUpperCase().split("")
     document.getElementById("category").innerHTML = categoryObject.category.toUpperCase()
-    console.log(typeof word);
 
     for (let i = 0; i < word.length; i++) {
         wordVisual.push(`&nbsp_&nbsp`)
@@ -50,48 +53,48 @@ function useLetter(letter) {
         for (let i = 0; i < word.length; i++) {
             if (letter === word[i]) {
                 wordVisual[i] = letter
-                wordVisualF()                
+                changeLetterVisual()
             }
         }
         document.getElementById(`letter${letter}`).style = "background: green;"
     } else {
-        contForca = contForca + 1
-        document.querySelector(".forca").src = `img/f${contForca}.png`
+        countForca = countForca + 1
+        document.querySelector(".forca").src = `img/f${countForca}.png`
     }
 
-    if (contForca === ATTEMPTS_NUMBER) {
-        document.querySelector("#pcWordGameOver").innerHTML = word.join("") 
+    if (countForca === MAX_FORCA) {
+        document.querySelector("#pcWordGameOver").innerHTML = word.join("")
         document.querySelector("#modalGameOver").style.display = "block"
     }
 
     if (word.join("") == wordVisual.join("")) {
-        document.location.reload(true)
+        console.log("Acabou");
+        
     }
 }
 
 // Substitui letra tentada por visualWord
-function wordVisualF() {
-    document.getElementById("pcWord").innerHTML=""   
+function changeLetterVisual() {
+    document.getElementById("pcWord").innerHTML = ""
+
     for (let i = 0; i < wordVisual.length; i++) {
         document.getElementById("pcWord").innerHTML += wordVisual[i]
     }
 }
 
-// Imagem da forca
-let contForca = 0
-document.querySelector(".forca").src = `img/f${contForca}.png`
+// Mudar imagem da forca
+document.querySelector(".forca").src = `img/f${countForca}.png`
 
 // Função escolher nomes dos jogadores
-function jogar() {
+function startGame() {
     if (document.querySelector('#inputName1').value === "" || document.querySelector('#inputName2').value === "") {
         alert("Por Favor Escolhe o Teu Nome!")
     } else {
-        document.querySelector("#modalNomes").style.display = "none"
-        pName1 = document.querySelector("#inputName1").value
-        pName2 = document.querySelector("#inputName2").value
-        document.querySelector(".leftP").innerHTML = pName1
-        document.querySelector(".rightP").innerHTML = pName2
+        document.querySelector("#modalNames").style.display = "none"
+        playerName1 = document.querySelector("#inputName1").value
+        playerName2 = document.querySelector("#inputName2").value
+        document.querySelector(".leftP").innerHTML = playerName1
+        document.querySelector(".rightP").innerHTML = playerName2
     }
 }
-document.querySelector(".btnInputNames").addEventListener("click", jogar);
-
+document.querySelector(".btnInputNames").addEventListener("click", startGame);
